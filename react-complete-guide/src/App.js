2feec3c0337a5a6a434b92049eea8cf18ff230e9
Edely <edely.gomes@convergenceworks.com.br>
2018-11-16 12:20:08 -0300

    45 - Changing state using input and event.target.value

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -24,6 +24,16 @@ class App extends Component {
     })
   }
 
+  nameChangedHandler = (event) =>{
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -36,7 +46,8 @@ class App extends Component {
           <Person 
             name={this.state.persons[1].name} 
             age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Martinália')}>My hobbies: Racing </Person>
+            click={this.switchNameHandler.bind(this, 'Martinália')}
+            changed={this.nameChangedHandler}>My hobbies: Racing </Person>
           <Person 
             name={this.state.persons[2].name} 
             age={this.state.persons[0].age}/>
