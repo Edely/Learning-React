982a2e994464ecca7f567586bcad9302f7ba2037
Edely <edely.gomes@convergenceworks.com.br>
2018-11-16 10:34:22 -0300

    30 - React.createElement vs render - nested elements - JSx

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
           <h1>Hi, I'm a React App</h1>
       </div>
     );
+
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does It Work now?'));
   }
 }
 
