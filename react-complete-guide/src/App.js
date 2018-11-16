a88ae39ccb95f3980720bc83df0176e7239b1811
Edely <edely.gomes@convergenceworks.com.br>
2018-11-16 10:49:37 -0300

    32 - Components are functions. How to export - import a component to app.js

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <h1>Hi, I'm a React App</h1>
-          <p>This is really working!</p>
+          <p>This is really working!</p>          
+          <Person />
       </div>
     );
 
