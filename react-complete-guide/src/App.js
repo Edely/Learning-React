0f427948abb0bf0a2a124c018c7aecbdd9d660c6
Edely <edely.gomes@convergenceworks.com.br>
2018-11-16 12:46:35 -0300

    47 - Adding scoped styling in element (render)

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -35,11 +35,22 @@ class App extends Component {
   }
 
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
           <h1>Hi, I'm a React App</h1>
           <p>This is really working!</p>    
-          <button onClick={() => this.switchNameHandler('Alexandro!')}>Switch Name</button>      
+          <button 
+            style={style}
+            onClick={() => this.switchNameHandler('Alexandro!')}>Switch Name</button>      
           <Person 
             name={this.state.persons[0].name} 
             age={this.state.persons[0].age}/>
