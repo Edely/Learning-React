b304a0d90af78525b4454e92a4680a1a6a8978ad
Edely <edely.gomes@convergenceworks.com.br>
2018-11-16 12:10:12 -0300

    44 - Usando handler no app js - com bind e com arrow function

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () =>{
+  switchNameHandler = (newName) =>{
     //DONT DO THIS this.state.persons[0].name = 'Maximilian';
 
     this.setState({
       persons: [
-        { name: 'Maximilian', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
       ]
@@ -29,10 +29,17 @@ class App extends Component {
       <div className="App">
           <h1>Hi, I'm a React App</h1>
           <p>This is really working!</p>    
-          <button onClick={this.switchNameHandler}>Switch Name</button>      
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-          <Person name= {this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing </Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[0].age}/>
+          <button onClick={() => this.switchNameHandler('Alexandro!')}>Switch Name</button>      
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}/>
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Martinália')}>My hobbies: Racing </Person>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[0].age}/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does It Work now?'));
