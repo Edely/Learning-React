b49bd876840cccc511f5d476d8ce02701b93796a
Edely <edely.gomes@convergenceworks.com.br>
2018-11-16 11:21:48 -0300

    37 - Passing content using props.children - content inside de <Component> ... </Component>

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
           <h1>Hi, I'm a React App</h1>
           <p>This is really working!</p>          
           <Person name="Max" age ="28"/>
-          <Person name= "Manu" age="29"/>
-          <Person name="Stephani" age="26"> My hobbies: Racing </Person>
+          <Person name= "Manu" age="29">My hobbies: Racing </Person>
+          <Person name="Stephani" age="26"/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does It Work now?'));
