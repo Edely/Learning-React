31bb9c565c41cfa39d32abfa3611224eb00bb99a
Edely <edely.gomes@convergenceworks.com.br>
2018-11-16 11:31:38 -0300

    38 - Using state (object property in class) to pass data to component rendering

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
           <h1>Hi, I'm a React App</h1>
-          <p>This is really working!</p>          
-          <Person name="Max" age ="28"/>
-          <Person name= "Manu" age="29">My hobbies: Racing </Person>
-          <Person name="Stephani" age="26"/>
+          <p>This is really working!</p>    
+          <button>Switch Name</button>      
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+          <Person name= {this.state.persons[1].name} age={this.state.persons[0].age}>My hobbies: Racing </Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[0].age}/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does It Work now?'));
