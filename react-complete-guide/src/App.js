1c0d2a3d6c034e3d67322b0b445b9db7c4af15e1
Edely <edely.gomes@convergenceworks.com.br>
2018-11-20 10:56:28 -0300

    57 - Flexible lists - using spread operator

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -26,14 +26,24 @@ class App extends Component {
     })
   }
 
-  nameChangedHandler = (event) =>{
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    })
+  nameChangedHandler = (event, id) =>{
+
+    const personIndex = this.state.persons.findIndex(p =>{
+      return p.id === id;
+    });
+
+    //const person = Object.assign({}, this.state.persons[personIndex]);
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons })
   }
 
   deletePersonHandler = (personIndex) =>{
@@ -68,7 +78,8 @@ class App extends Component {
                     click={() => this.deletePersonHandler(index)}
                     name={person.name} 
                     age={person.age}
-                    key={person.id}/>
+                    key={person.id}
+                    changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
         </div>
       );
