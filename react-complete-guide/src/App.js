8e597c572ee0c7866ea545b8fdaa58cd5f5152f4
Edely <edely.gomes@convergenceworks.com.br>
2018-11-20 10:12:02 -0300

    53 - Listing elements with map and arrow function

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -37,7 +37,6 @@ class App extends Component {
   }
 
   togglePersonsHandler = () =>{
-    console.log('clicked');
     const doesShow = this.state.showPersons;
     this.setState({showPersons: !doesShow});
   }
@@ -57,17 +56,11 @@ class App extends Component {
     if(this.state.showPersons){
       persons = (
         <div>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}/>
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Martinália')}
-          changed={this.nameChangedHandler}>My hobbies: Racing </Person>
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[0].age}/>
+          {this.state.persons.map(person => {
+            return <Person 
+                    name={person.name} 
+                    age={person.age}/>
+          })}
         </div>
       );
     }
