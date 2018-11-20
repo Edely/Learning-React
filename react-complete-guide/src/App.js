a868b4b1aaa93c3977cc20ad394492241c388529
Edely <edely.gomes@convergenceworks.com.br>
2018-11-20 10:02:10 -0300

    51 - More elegant way to use conditionals - with a variable(null)

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -51,6 +51,26 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     };
+          
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons = (
+        <div>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Martinália')}
+          changed={this.nameChangedHandler}>My hobbies: Racing </Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[0].age}/>
+        </div>
+      );
+    }
 
     return (
       <div className="App">
@@ -58,26 +78,8 @@ class App extends Component {
           <p>This is really working!</p>    
           <button 
             style={style}
-            onClick={this.togglePersonsHandler}>Switch Name</button>      
-          
-          {
-            this.state.showPersons ?
-            <div>
-            <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age}/>
-            <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Martinália')}
-              changed={this.nameChangedHandler}>My hobbies: Racing </Person>
-            <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[0].age}/>
-            </div>:
-            null
-          }
-          
+            onClick={this.togglePersonsHandler}>Switch Name</button>     
+          {persons}
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does It Work now?'));
