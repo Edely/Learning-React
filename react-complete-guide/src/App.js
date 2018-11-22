cc6e763aa71081cc9f15023132f20bd18479e30a
Edely <edely.gomes@convergenceworks.com.br>
2018-11-22 09:25:50 -0300

    76 - Adding ErrorBoundary as a component and wrapping Person on it

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 
@@ -67,12 +68,13 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person 
+            return <ErrorBoundary key={person.id}> 
+                    <Person 
                     click={() => this.deletePersonHandler(index)}
                     name={person.name} 
                     age={person.age}
-                    key={person.id}
                     changed={(event) => this.nameChangedHandler(event, person.id)}/>
+                    </ErrorBoundary>
           })}
         </div>
       );
