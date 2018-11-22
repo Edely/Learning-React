0712854d4561d8160c9f62434a41d14414c92161
Edely <edely.gomes@convergenceworks.com.br>
2018-11-22 09:34:54 -0300

    Remove Error codes to come go with the course

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 
@@ -68,13 +67,12 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}> 
-                    <Person 
+            return <Person 
                     click={() => this.deletePersonHandler(index)}
                     name={person.name} 
                     age={person.age}
+                    key={person.id}
                     changed={(event) => this.nameChangedHandler(event, person.id)}/>
-                    </ErrorBoundary>
           })}
         </div>
       );
