b9b192a4d514845ffe677c717064c23d90a6d2cf
Edely <edely.gomes@convergenceworks.com.br>
2018-11-26 12:37:42 -0300

    96 - Using hoc to classes - withclass

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
-import Cockpit from '../components/Cockpit/Cockpit'
+import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends PureComponent {
 
@@ -111,7 +112,7 @@ class App extends PureComponent {
     }
 
     return(      
-      <div className={ classes.App }>
+      <WithClass classes={classes.App}>
           <button onClick={ () => { this.setState({showPersons: true}) } } >Show Persons</button>
           <Cockpit 
             appTitle={ this.props.title }
@@ -120,7 +121,7 @@ class App extends PureComponent {
             clicked={ this.togglePersonsHandler }
           />
           {persons}
-      </div>
+      </WithClass>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does It Work now?'));
   }
