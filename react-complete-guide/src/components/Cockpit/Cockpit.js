a2d86ca6524e507fe2d08bf0960ee4ec9c83873d
Edely <edely.gomes@convergenceworks.com.br>
2018-11-26 12:25:52 -0300

    94 - Adding a auxiliary component as a wrapper 'element' or using an array

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import classes from './Cockpit.css';
+import Aux from '../../hoc/Auxiliary/Auxiliary';
 
 const cockpit = (props) =>{
 
     
     const assignedClasses = [];
-    let btnClass = '';
+    let btnClass = classes.Button;
 
     if(props.showPersons){
-        btnClass = classes.Red;
+        btnClass = [classes.Button, classes.Red].join(' ');
     }    
 
     if(props.persons.length <= 2){
@@ -20,13 +21,13 @@ const cockpit = (props) =>{
     }
 
     return(
-        <div className={classes.Cockpit}>
+        <Aux>
             <h1>{ props.appTitle }</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>    
             <button 
                 className={btnClass}
                 onClick={props.clicked}>Toggle Persons</button>
-        </div>     
+        </Aux>     
     );
 }
 
