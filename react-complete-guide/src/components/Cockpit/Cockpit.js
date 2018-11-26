9157b879bd755a0abcca835f941092ea554a0033
Edely <edely.gomes@convergenceworks.com.br>
2018-11-26 12:55:15 -0300

    Reorganiza a  pasta hoc

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './Cockpit.css';
-import Aux from '../../hoc/Auxiliary/Auxiliary';
+import Aux from '../../hoc/Auxiliary';
 
 const cockpit = (props) =>{
 
