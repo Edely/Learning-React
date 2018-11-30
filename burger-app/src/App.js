0784881dfebf05b92943f4017f255e0c8582a304
Edely <edely.gomes@convergenceworks.com.br>
2018-11-30 14:28:09 -0300

    116 - Adds styling and the BurgerBuilder Container

--- burger-app/src/App.js
+++ burger-app/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+
 import Layout from './components/Layout/Layout';
+import BurguerBuild from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <BurguerBuild />
         </Layout>
       </div>
     );
