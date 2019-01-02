1007b126ed849a62226ea85115df9731301ee120
Edely <edely.gomes@convergenceworks.com.br>
2019-01-02 10:46:40 -0300

    126 - Adding Price

--- burger-app/src/components/Burger/BuildControls/BuildControls.js
+++ burger-app/src/components/Burger/BuildControls/BuildControls.js
@@ -13,6 +13,7 @@ const controls = [
 //{ salad: true, meat: false, ... }
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
             <BuildControl
                 key={ctrl.label}
--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -75,7 +75,8 @@ class BurgerBuilder extends Component {
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
-                    disabledInfo={disabledInfo} />
+                    disabledInfo={disabledInfo}
+                    price={this.state.totalPrice} />
             </Aux>
         );
     }
