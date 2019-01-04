7ba1d83488b40c870424ad08960a774d3c1aa3fc
Edely <edely.gomes@convergenceworks.com.br>
2019-01-04 09:32:06 -0300

    133 - Adiciona valor total da compra

--- burger-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -16,6 +16,8 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+
+            <p>Total Price: <strong>{props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
             <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
             <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
