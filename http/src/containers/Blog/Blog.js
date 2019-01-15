5ff45ff0f828ce80b3323544d3eef0d9a02d061a
Edely <edely.gomes@convergenceworks.com.br>
2019-01-15 10:22:17 -0300

    155 - Transforming data

--- http/src/containers/Blog/Blog.js
+++ http/src/containers/Blog/Blog.js
@@ -18,7 +18,15 @@ class Blog extends Component {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(
                 response => {
-                    this.setState({ posts: response.data });
+                    const posts = response.data.slice(0, 4);
+                    const updatedPosts = posts.map(post => {
+                        return {
+                            ...post,
+                            author: 'Max'
+                        }
+
+                    })
+                    this.setState({ posts: updatedPosts });
                     //console.log(response)
                 }
             );
@@ -27,7 +35,9 @@ class Blog extends Component {
     render() {
 
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} />
+            return <Post key={post.id}
+                title={post.title}
+                author={post.author} />
         });
         return (
             <div>
