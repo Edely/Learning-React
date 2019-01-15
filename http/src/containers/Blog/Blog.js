cbff23c80554ad2c64e5d2f4978ca9407faff8e3
Edely <edely.gomes@convergenceworks.com.br>
2019-01-15 15:24:54 -0300

    161 - Handling errors localy

--- http/src/containers/Blog/Blog.js
+++ http/src/containers/Blog/Blog.js
@@ -9,10 +9,9 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: [
-
-        ],
-        selectedPostId: null
+        posts: [],
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
@@ -30,6 +29,12 @@ class Blog extends Component {
                     this.setState({ posts: updatedPosts });
                     //console.log(response)
                 }
+            )
+            .catch(
+                error => {
+                    this.setState({ error: true })
+                    console.log(error);
+                }
             );
     }
 
@@ -38,14 +43,17 @@ class Blog extends Component {
     }
 
     render() {
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)
+                    } />
+            });
+        }
 
-        const posts = this.state.posts.map(post => {
-            return <Post key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)
-                } />
-        });
         return (
             <div>
                 <section className="Posts">
