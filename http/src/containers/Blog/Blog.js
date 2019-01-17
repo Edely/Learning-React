99cee7647c861075624b469103883072655b8060
Edely <edely.gomes@convergenceworks.com.br>
2019-01-17 10:39:52 -0300

    182 - Preparing the component for routing

--- http/src/containers/Blog/Blog.js
+++ http/src/containers/Blog/Blog.js
@@ -1,57 +1,11 @@
 import React, { Component } from 'react';
-import axios from '../../axios';
-
 
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
 
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    componentDidMount() {
-        axios.get('/posts')
-            .then(
-                response => {
-                    const posts = response.data.slice(0, 4);
-                    const updatedPosts = posts.map(post => {
-                        return {
-                            ...post,
-                            author: 'Max'
-                        }
-
-                    })
-                    this.setState({ posts: updatedPosts });
-                    //console.log(response)
-                }
-            )
-            .catch(
-                error => {
-                    this.setState({ error: true })
-                    console.log(error);
-                }
-            );
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({ selectedPostId: id })
-    }
-
     render() {
-        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)
-                    } />
-            });
-        }
-
         return (
             <div className="Blog">
                 <header>
@@ -62,10 +16,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-
+                <Posts />
             </div >
         );
     }
