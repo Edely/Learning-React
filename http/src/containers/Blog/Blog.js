ef64e9b0da90d25e58d74547a192e4d24cd067bd
Edely <edely.gomes@convergenceworks.com.br>
2019-01-17 10:55:15 -0300

    186 - Switching Between Pages

--- http/src/containers/Blog/Blog.js
+++ http/src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
 
@@ -16,8 +17,12 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={() => <h1>Home</h1>} />
-                <Route path="/" render={() => <h1>Home2</h1>} />
+
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home2</h1>} /> */}
+
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div >
         );
     }
