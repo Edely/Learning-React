576c2cdd3dbb4afaad3b21d230e5d0c927db09cf
Edely <edely.gomes@convergenceworks.com.br>
2019-01-17 09:03:14 -0300

    181 - Setting Up Router Package

--- http/src/containers/Blog/Blog.js
+++ http/src/containers/Blog/Blog.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+
 import './Blog.css';
 
 class Blog extends Component {
@@ -55,16 +53,19 @@ class Blog extends Component {
         }
 
         return (
-            <div>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
                 </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+
             </div >
         );
     }
