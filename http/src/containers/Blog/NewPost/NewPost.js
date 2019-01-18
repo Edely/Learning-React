10a22734cf8cbd6b88b7765b9d812f980bf23d94
Edely <edely.gomes@convergenceworks.com.br>
2019-01-18 09:42:56 -0300

    203 - Using the History Prop to Redirect Replace

--- http/src/containers/Blog/NewPost/NewPost.js
+++ http/src/containers/Blog/NewPost/NewPost.js
@@ -24,7 +24,8 @@ class NewPost extends Component {
             .then(
                 response => {
                     console.log(response);
-                    this.setState({ submitted: true })
+                    this.props.history.replace('/posts');
+                    //this.setState({ submitted: true })
                 });
     }
 
