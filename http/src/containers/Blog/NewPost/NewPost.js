457c1eec38121ac74861e551bf14c6c3532ac2fa
Edely <edely.gomes@convergenceworks.com.br>
2019-01-18 09:37:11 -0300

    202 - Conditional Redirects

--- http/src/containers/Blog/NewPost/NewPost.js
+++ http/src/containers/Blog/NewPost/NewPost.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import { Redirect } from 'react-router-dom';
 import './NewPost.css';
 
 class NewPost extends Component {
@@ -23,13 +23,19 @@ class NewPost extends Component {
         axios.post('/posts', data)
             .then(
                 response => {
-                    console.log(response)
+                    console.log(response);
+                    this.setState({ submitted: true })
                 });
     }
 
     render() {
+        let redirect = null;
+        if (this.state.submitted) {
+            redirect = <Redirect to="/posts" />
+        }
         return (
             <div className="NewPost">
+                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({ title: event.target.value })} />
