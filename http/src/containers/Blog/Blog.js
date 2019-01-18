7ca532e6658be091453c747f59c756686752fc73
Edely <edely.gomes@convergenceworks.com.br>
2019-01-18 09:20:48 -0300

    200 - Creating Dynamic Nested Routes

--- http/src/containers/Blog/Blog.js
+++ http/src/containers/Blog/Blog.js
@@ -34,7 +34,7 @@ class Blog extends Component {
 
                 <Switch>
                     <Route path="/new-post" component={NewPost} />
-                    <Route path="/" component={Posts} />
+                    <Route path="/posts" component={Posts} />
                 </Switch>
             </div >
         );
--- http/src/containers/Blog/FullPost/FullPost.js
+++ http/src/containers/Blog/FullPost/FullPost.js
@@ -10,8 +10,17 @@ class FullPost extends Component {
     }
 
     componentDidMount() {
+        console.log(this.props);
+        this.loadData();
+    }
+
+    componentDidUpdate() {
+        this.loadData();
+    }
+
+    loadData() {
         if (this.props.match.params.id) {
-            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.match.params.id)) {
                 axios.get("/posts/" + this.props.match.params.id)
                     .then(response => {
                         this.setState({ loadedPost: response.data });
@@ -21,7 +30,7 @@ class FullPost extends Component {
     }
 
     deletePostHandler = () => {
-        axios.delete('/posts/' + this.props.id)
+        axios.delete('/posts/' + this.props.match.params.id)
             .then(response => {
                 console.log(response)
             });
@@ -29,7 +38,7 @@ class FullPost extends Component {
 
     render() {
         let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
-        if (this.props.id) {
+        if (this.props.match.params.id) {
             post = <p style={{ textAlign: 'center' }}>Loading!</p>;
         }
         if (this.state.loadedPost) {
@@ -41,7 +50,6 @@ class FullPost extends Component {
                         <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
-
             );
 
         }
