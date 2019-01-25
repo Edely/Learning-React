b34586288b43b4b6ce0ac15f1bb6092938436b26
Edely <edely.gomes@convergenceworks.com.br>
2019-01-25 10:38:26 -0300

    Assignment 4 - Dispatching Actions

--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -9,6 +9,24 @@ const reducer = (state = initialState, action) => {
         }
     }
 
+    if (action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+
+    if (action.type === 'ADD') {
+        return {
+            counter: state.counter + 5
+        }
+    }
+
+    if (action.type === 'SUBTRACT') {
+        return {
+            counter: state.counter - 5
+        }
+    }
+
     return state;
 }
 
