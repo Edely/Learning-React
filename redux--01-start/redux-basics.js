e8fc1e75fd2347e3e97e159fd123f0789a80f656
Edely <edely.gomes@convergenceworks.com.br>
2019-01-25 09:18:44 -0300

    247 - Dispatching Actions

--- redux--01-start/redux-basics.js
+++ redux--01-start/redux-basics.js
@@ -7,6 +7,18 @@ const initialState = {
 
 // REDUCER
 const rootReducer = (state = initialState, action) => {
+    if (action.type == 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+    if (action.type == 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
     return state;
 };
 
@@ -15,5 +27,8 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Action
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+console.log(store.getState());
 
 // Subscription
