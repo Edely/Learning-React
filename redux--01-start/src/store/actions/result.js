057eec94bd27bb91b692ebf9a8b46ba95c3b737c
Edely <edely.gomes@convergenceworks.com.br>
2019-01-31 10:52:14 -0300

    282 - Using Action Creators and Get State

--- redux--01-start/src/store/actions/result.js
+++ redux--01-start/src/store/actions/result.js
@@ -9,9 +9,11 @@ export const saveResult = (res) => {
 };
 
 export const storeResult = (res) => {
-    return dispatch => {
+    return (dispatch, getState) => {
         setTimeout(() => {
-            dispatch(saveResult(res))
+            // const oldCounter = getState().ctr.counter;
+            // console.log(oldCounter);
+            dispatch(saveResult(res));
         }, 2000);
     }
 };
