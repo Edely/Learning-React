f57e59078f673a7885a487302396d82b644a5e91
Edely <edely.gomes@convergenceworks.com.br>
2019-01-31 11:26:11 -0300

    284 - A LEaner Switch Case Statement

--- redux--01-start/src/store/reducers/result.js
+++ redux--01-start/src/store/reducers/result.js
@@ -5,23 +5,19 @@ const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, {
+        results: updatedArray
+    })
+}
+
 const reducer = (state = initialState, action) => {
 
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            //Change data
-            return updateObject(state,
-                {
-                    results: state.results.concat({ id: new Date(), value: action.result * 2 })
-                })
-        case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const newArray = [...state.results];
-            // newArray.splice(id, 1);
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return updateObject(state, {
-                results: updatedArray
-            })
+            return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result * 2 }) });
+        case actionTypes.DELETE_RESULT: return deleteResult(state, action)
         default:
             return state;
     }
