63ab6e40f8989f817d6e6b5181ee1a0e8ce24b75
Edely <edely.gomes@convergenceworks.com.br>
2019-02-01 10:14:41 -0300

    301 - Redirect To Improve UX

--- burger-app/src/containers/Checkout/Checkout.js
+++ burger-app/src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
@@ -15,15 +15,24 @@ class Checkout extends Component {
     }
 
     render() {
+        let summary = <Redirect to="/" />
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary
+                        ingredients={this.props.ings}
+                        checkoutCancelled={this.checkoutCancelledHandler}
+                        checkoutContinued={this.checkoutContinuedHandler} />
+                    <Route
+                        path={this.props.match.path + '/contact-data'}
+                        component={ContactData} />
+                </div>
+            );
+        }
         return (
             <div>
-                <CheckoutSummary
-                    ingredients={this.props.ings}
-                    checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler} />
-                <Route
-                    path={this.props.match.path + '/contact-data'}
-                    component={ContactData} />
+                {summary}
+
             </div>
         );
     }
