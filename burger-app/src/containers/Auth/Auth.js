96d4aed2bdcd95ea49888e12f908d736e4394bb9
Edely <edely.gomes@convergenceworks.com.br>
2019-02-04 13:39:49 -0300

    319 - Adding a Spinner

--- burger-app/src/containers/Auth/Auth.js
+++ burger-app/src/containers/Auth/Auth.js
@@ -4,6 +4,7 @@ import Button from '../../components/UI/Button/Button';
 import Classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 import { connect } from 'react-redux';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Auth extends Component {
 
@@ -107,7 +108,7 @@ class Auth extends Component {
             });
         }
 
-        const form = formElementsArray.map(formElement => (
+        let form = formElementsArray.map(formElement => (
             <Input
                 key={formElement.id}
                 elementType={formElement.config.elementType}
@@ -120,8 +121,19 @@ class Auth extends Component {
 
         ));
 
+        if (this.props.loading) {
+            form = <Spinner />;
+        }
+
+        let errorMessage = null;
+        if (this.props.error) {
+            errorMessage = (
+                <p>{this.props.error.message}</p>
+            );
+        }
         return (
             <div className={Classes.Auth}>
+                {errorMessage}
                 <form onSubmit={(event) => this.submitHandler(event)}>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
@@ -134,11 +146,17 @@ class Auth extends Component {
     };
 }
 
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error
+    };
+};
 
 const mapDispatchToProps = dispacth => {
     return {
         onAuth: (email, password, isSignup) => dispacth(actions.auth(email, password, isSignup))
-    }
-}
+    };
+};
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
