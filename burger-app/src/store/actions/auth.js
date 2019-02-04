9849ce61121c83c1fb07e5036e5b183a5c022087
Edely <edely.gomes@convergenceworks.com.br>
2019-02-04 13:56:47 -0300

    320 - Loggin Users Out

--- burger-app/src/store/actions/auth.js
+++ burger-app/src/store/actions/auth.js
@@ -16,7 +16,6 @@ export const authSuccess = (token, userId) => {
     };
 };
 
-
 export const authFail = (error) => {
     return {
         type: actionTypes.AUTH_FAIL,
@@ -24,6 +23,19 @@ export const authFail = (error) => {
     };
 };
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000);
+    };
+};
 
 export const auth = (email, password, isSignup) => {
     return dispatch => {
@@ -44,6 +56,7 @@ export const auth = (email, password, isSignup) => {
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(err => {
                 dispatch(authFail(err.response.data.error));
