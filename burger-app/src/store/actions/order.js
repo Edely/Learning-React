7af68fc94d1db0aaa42eed5d28ffccc1233248ee
Edely <edely.gomes@convergenceworks.com.br>
2019-02-06 16:10:54 -0300

    Displaying User Specific Orders

--- burger-app/src/store/actions/order.js
+++ burger-app/src/store/actions/order.js
@@ -69,10 +69,11 @@ export const fetchOrdersStart = () => {
 };
 
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
     return dispatch => {
-        dispatch(fetchOrdersStart())
-        axios.get('/orders.json?auth=' + token)
+        dispatch(fetchOrdersStart());
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo=' + '"' + userId + '"';
+        axios.get('/orders.json' + queryParams)
             .then(
                 res => {
                     const fetchedOrders = [];
