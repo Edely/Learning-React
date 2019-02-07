74ed412b16a4e0d3d90a7d07e81429e2bc69cebf
edely <edely.gomes@gmail.com>
2019-02-07 20:28:26 -0300

    adiciona estrutura básica e state

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    tarefas: [
+      {
+        tarefa: 'Lavar os Pratos',
+        descricao: 'A melhor das tarefas. Quem não gosta?!!!',
+        responsavel: 'Edely',
+        prazo: '11/02/90'
+      },
+      {
+        tarefa: 'Alimentar os Gatos',
+        descricao: 'São uns monstros. Não demore!',
+        responsavel: 'Selina',
+        prazo: '25/12/2020'
+      }
+    ]
+  }
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className={'main'}>
+        <header className={'header'}>
+          Tarefas Domésticas
         </header>
+        <ul className={'lista-tarefas'}>
+          <li className={'lista-tarefa__itens'}>
+            <span>Lavar os Pratos </span>
+            <span>Selina </span>
+            <span>11/02/90</span>
+          </li>
+        </ul>
       </div>
     );
   }
 }
+
 
 export default App;
