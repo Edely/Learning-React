c7cff52ffeefd3019e55875ba7337c189cb7699b
Edely <edely.gomes@convergenceworks.com.br>
2019-02-07 11:10:48 -0300

    testing components correctly

--- burger-app/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ burger-app/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -25,7 +25,7 @@ describe('<NavigationItems />', () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 
-    it('should render logout <NavigationItem />', () => {
+    it('should an exact logout button', () => {
         wrapper.setProps({ isAuthenticated: true });
         expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
     });
