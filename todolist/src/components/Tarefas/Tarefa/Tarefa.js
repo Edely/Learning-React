b6656365a17bc62038e169684a47f5db6472a0c4
edely <edely.gomes@gmail.com>
2019-02-09 21:30:41 -0300

    adiciona novas classes para as infos dos itens das tarefas

--- todolist/src/components/Tarefas/Tarefa/Tarefa.js
+++ todolist/src/components/Tarefas/Tarefa/Tarefa.js
@@ -12,11 +12,10 @@ const tarefa = (props) =>{
     console.log(props)
     return(
         <div className={'lista-tarefa__itens'}>
-            <div className={'lista-tarefa__itens__info'}>{ date }</div>
-            <div className={'lista-tarefa__itens__info'}>{props.nome} </div>
-            <div className={'lista-tarefa__itens__info'}>{props.descricao}</div>            
-            <div className={'lista-tarefa__itens__info'}>{props.responsavel}</div>
-                      
+            <div className={'lista-tarefa__itens__info lista-tarefa__itens__info--name'}>{props.nome} </div>
+            <div className={'lista-tarefa__itens__info lista-tarefa__itens__info--date'}>{ date }</div>
+            <div className={'lista-tarefa__itens__info lista-tarefa__itens__info--desc'}>{props.descricao}</div>            
+            <div className={'lista-tarefa__itens__info lista-tarefa__itens__info--resp'}>{props.responsavel}</div>
             <div className={'lista-tarefa__itens__info'}><Button>Eas</Button></div>
         </div>
     );
